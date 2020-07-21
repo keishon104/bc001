6cdbea13ea7a126cedaf7f7346293f66fae0c470
Keishon <keishon104@gmail.com>
2020-07-21 09:20:04 -0400

    Submits Initial App
    The single page React app completed according to the specifications of the Galvanize videos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// Data  
+const fruit_list = [
+  {id: 4, article: "fruit", fruit_name: "apple"},
+  {id: 5, article: "fruit", fruit_name: "orange"},
+  {id: 7, article: "fruit", fruit_name: "pears"}
+
+]
+
+// Compoenents
+const Headder = () => <div className="header"> This is the header </div>
+const Footter = () => <div className="footer"> This is the Footer </div>
+const Body = () => <div className="header"> {fruit_list.map(fruit => <Fruit key={fruit.id} article={fruit.article} fruit_name={fruit.fruit_name}/> )} </div>
+
+
+
+const Fruit = ({article, fruit_name}) => <div>I am a {article} called a {fruit_name} </div>
+
+
+// Return Function
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headder/>
+      <Body/>
+      <Footter/>
     </div>
   );
 }
